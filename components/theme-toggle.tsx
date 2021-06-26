6335f63b5729f9ff5936e81a1d08d3eb4ee4e165
Alex Marshall <alex.k.marshall83@gmail.com>
2021-06-26 19:09:58 +0100

    Add basic theme toggle switch

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,22 +1,73 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useMemo, useState } from 'react'
 
-type Theme = 'light' | 'dark'
+const themes = ['light', 'dark'] as const
+type Theme = typeof themes[number]
+
+function getLocalStorageTheme(): Theme | null {
+  const maybeTheme: unknown = window.localStorage.getItem('theme')
+  const theme = themes.find((theme) => theme === maybeTheme)
+  if (theme) return theme
+  return null
+}
+
+function getMediaQueryTheme(): Theme | null {
+  const mql = window.matchMedia('(prefers-color-scheme: dark)')
+  const hasMediaQueryPreference = typeof mql.matches === 'boolean'
+
+  if (hasMediaQueryPreference) {
+    return mql.matches ? 'dark' : 'light'
+  }
+
+  return null
+}
 
 export function ThemeToggle() {
-  const [theme, setTheme] = useState<Theme>('light')
+  const [savedPreference, setSavedPreference] = useState<Theme | null>(null)
+  const [mediaQueryPreference, setMediaQueryPreference] =
+    useState<Theme | null>(null)
 
-  useEffect(() => {
-    document.body.dataset.theme = theme
-  }, [theme])
+  console.log({ savedPreference })
+  console.log({ mediaQueryPreference })
+
+  useEffect(function initialiseFromLocalStorage() {
+    const maybeSavedPreference = getLocalStorageTheme()
+    if (maybeSavedPreference) setSavedPreference(maybeSavedPreference)
+  }, [])
+
+  useEffect(function syncMediaQueryPreference() {
+    const maybeMQPreference = getMediaQueryTheme()
+    if (maybeMQPreference) setMediaQueryPreference(maybeMQPreference)
+  }, [])
+
+  useEffect(
+    function syncDocumentTheme() {
+      if (savedPreference) {
+        document.documentElement.dataset.userTheme = savedPreference
+      }
+    },
+    [savedPreference]
+  )
+
+  useEffect(
+    function syncThemeLocalStorage() {
+      if (savedPreference) {
+        window.localStorage.setItem('theme', savedPreference)
+      }
+    },
+    [savedPreference]
+  )
+
+  const currentTheme = savedPreference ?? mediaQueryPreference ?? 'light'
+
+  console.log({ currentTheme })
+
+  function toggleTheme() {
+    setSavedPreference(currentTheme === 'light' ? 'dark' : 'light')
+  }
 
   return (
-    <button
-      type="button"
-      onClick={() =>
-        setTheme((theme) => (theme === 'light' ? 'dark' : 'light'))
-      }
-    >
-      {theme === 'light' ? 'moon' : 'sun'}
+    <button type="button" onClick={() => toggleTheme()}>
+      {currentTheme === 'light' ? 'moon' : 'sun'}
     </button>
   )
 }
---
+++ pages/_document.tsx
@@ -0,0 +1,43 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head />
+        <body>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: blockingSetInitialColorMode,
+            }}
+          ></script>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
+
+// our function needs to be a string
+const blockingSetInitialColorMode = `(function() {
+	${setInitialColorMode.toString()}
+	setInitialColorMode();
+})()
+`
+
+function setInitialColorMode() {
+  function getInitialColorMode() {
+    const persistedColorPreference = window.localStorage.getItem('theme')
+
+    if (persistedColorPreference) return persistedColorPreference
+  }
+
+  const colorMode = getInitialColorMode()
+  if (colorMode) {
+    const root = document.documentElement
+    root.setAttribute('data-theme', colorMode)
+  }
+}
+
+export default MyDocument
