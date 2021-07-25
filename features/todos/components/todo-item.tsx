0c387c6541c6276606cef6fa2a5b91856f25414b
Alex Marshall <alex.k.marshall83@gmail.com>
2021-07-25 13:34:12 +0100

    makes drag and drop animation smooth

--- features/todos/components/todo-item.tsx
+++ features/todos/components/todo-item.tsx
@@ -1,11 +1,11 @@
 import { motion } from 'framer-motion'
 import { useSortable } from '@dnd-kit/sortable'
 import { DeleteButton } from '@components/delete-button'
-import { CSS } from '@dnd-kit/utilities'
 import styles from '../todos.module.scss'
 import { Todo } from 'types/todo'
 import { useDeleteTodo, useToggleTodoComplete } from '../queries'
 import { Checkbox } from '@components/checkbox/checkbox'
+import { useTheme } from '@context/theme'
 
 type Props = {
   todo: Todo
@@ -25,22 +25,57 @@ export function TodoItem({ todo, onDeleteTodo }: Props) {
   const { setNodeRef, attributes, listeners, transform, isDragging } =
     useSortable({
       id: todo.id,
+      transition: null,
     })
+
+  const { theme } = useTheme()
+
+  const outlineColor =
+    theme === 'light' ? 'hsla(233, 11%, 84%, 100%)' : 'hsla(234, 11%, 52%)'
+  const shadowColor =
+    theme === 'light' ? 'hsla(236, 9%, 61%, 30%)' : 'hsla(0, 0%, 0%, 40%)'
+
+  const initialStyles = {
+    x: 0,
+    y: 0,
+    scale: 1,
+  }
 
   return (
     <motion.li
       className={styles.todoItem}
-      layout
-      animate={{ y: transform?.y }}
-      style={{
-        zIndex: isDragging ? 1 : undefined,
+      layoutId={todo.id}
+      animate={
+        transform
+          ? {
+              x: transform.x,
+              y: transform.y,
+              scale: isDragging ? 1.05 : 1,
+              zIndex: isDragging ? 1 : 0,
+              boxShadow: isDragging
+                ? `0 0 0 1px ${outlineColor}, 0px 15px 15px 0 ${shadowColor}`
+                : `0 0 0 0 ${outlineColor}, 0 0 0 0 ${shadowColor}`,
+            }
+          : initialStyles
+      }
+      transition={{
+        duration: !isDragging ? 0.25 : 0,
+        easings: { type: 'spring' },
+        scale: {
+          duration: 0.25,
+        },
+        zIndex: {
+          delay: isDragging ? 0 : 0.25,
+        },
       }}
     >
       <div ref={setNodeRef} {...attributes} {...listeners}>
         <motion.span
           className={styles.todoClipInWrapper}
-          initial={{ y: '120%' }}
-          animate={{ y: 0 }}
+          // initial={{ y: '120%' }}
+          // animate={{ y: 0 }}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
           exit={{ opacity: 0 }}
         >
           <Checkbox
