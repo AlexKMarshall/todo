3c05abf28c3493a47a764b6fad2fd28a352ad79d
Alex Marshall <alex.k.marshall83@gmail.com>
2021-08-01 18:54:45 +0100

    update todos feature to styled-components

--- features/todos/todos.tsx
+++ features/todos/todos.tsx
@@ -1,12 +1,13 @@
 import styled from 'styled-components'
 import { pluralise } from 'utils'
-import styles from './todos.module.scss'
 import { useNotification } from '@context/notification'
 import { Todo, TodoFilters } from './schemas'
 import { Link } from '@components/link'
 import { useClearCompletedTodos, useTodos } from './queries'
-import { CreateTodoForm as CreateTodoFormComponent } from './components/create-todo-form'
-import { TodoList as TodoListComponent } from './components/todo-list'
+import {
+  CreateTodoForm as CreateTodoFormComponent,
+  TodoList as TodoListComponent,
+} from './components'
 import { AnimatePresence } from 'framer-motion'
 
 type Props = {
@@ -32,19 +33,19 @@ export function Todos({ onDeleteTodo = () => {}, filters = {} }: Props) {
   const shouldShowClearCompleted = useShouldShowClearCompleted()
 
   return (
-    <div className={styles.appContent}>
+    <AppContent>
       <CreateTodoForm
         onCreateTodo={(todo) => setNotificationMessage(`${todo.title} added`)}
       />
-      <div className={styles.backgroundShadow} />
+      <BackgroundShadow />
       <TodoList onDeleteTodo={handleDeleteTodo} filters={filters} />
-      <div className={styles.itemsCount}>{activeTodosLeft}</div>
-      <div className={styles.filterLinks}>
+      <ItemsCount>{activeTodosLeft}</ItemsCount>
+      <FilterLinks>
         <Link href={`/todos`}>All</Link>
         <Link href={`/todos?status=active`}>Active</Link>
         <Link href="/todos?status=completed">Completed</Link>
-      </div>
-      <div className={styles.clearCompleted}>
+      </FilterLinks>
+      <ClearCompleted>
         <AnimatePresence>
           {shouldShowClearCompleted ? (
             <Link onClick={() => clearCompletedTodosMutation.mutate()}>
@@ -52,8 +53,8 @@ export function Todos({ onDeleteTodo = () => {}, filters = {} }: Props) {
             </Link>
           ) : null}
         </AnimatePresence>
-      </div>
-    </div>
+      </ClearCompleted>
+    </AppContent>
   )
 }
 
@@ -79,9 +80,116 @@ function useShouldShowClearCompleted() {
   return countCompletedTodosQuery.isSuccess && countCompletedTodosQuery.data > 0
 }
 
+const AppContent = styled.div`
+  --gutter: var(--s1);
+  flex: 1;
+  height: 0; /* otherwise 1fr grid row doesn't grow in chrome,
+  but this seems to mean the height doesn't respect the content when it overflows and so we don't get the bottom margin*/
+  display: grid;
+  width: 100%;
+  grid-template-columns: 1fr 1fr;
+  grid-template-rows: auto 1fr auto auto;
+  grid-template-areas:
+    'input input'
+    'list list'
+    'count clear'
+    'filters filters';
+
+  @media (min-width: 800px) {
+    grid-template-columns: minmax(max-content, 1fr) auto minmax(
+        max-content,
+        1fr
+      );
+    grid-template-rows: auto 1fr auto;
+    grid-template-areas:
+      'input input input'
+      'list list list'
+      'count filters clear';
+  }
+
+  & > * {
+    background-color: var(--main-background-color);
+  }
+`
+
 const CreateTodoForm = styled(CreateTodoFormComponent)`
   grid-area: input;
 `
 const TodoList = styled(TodoListComponent)`
   grid-area: list;
+`
+
+const BackgroundShadow = styled.div`
+  grid-row: 2 / 4;
+  grid-column: 1 / -1;
+  border-radius: var(--border-radius);
+
+  box-shadow: 0 0 30px 0px var(--shadow-color);
+  z-index: -1;
+
+  position: relative;
+  // TODO not sure how to make this sit behind the .backgroundShadow element
+  // &::before {
+  //   content: '';
+  //   position: absolute;
+  //   border-radius: var(--border-radius);
+  //   top: 0;
+  //   right: 20%;
+  //   bottom: 0;
+  //   left: 20%;
+  //   box-shadow: 0 0 50px 10px var(--shadow-color);
+  //   z-index: -1;
+  // }
+`
+
+const ItemsCount = styled.div`
+  grid-area: count;
+  border-bottom-left-radius: 4px;
+  padding: var(--s0) var(--s1);
+  font-size: max(var(--s-1), 15px);
+  color: var(--muted-text-color);
+`
+
+const FilterLinks = styled.div`
+  grid-area: filters;
+  margin-top: var(--gutter);
+  padding: var(--s0) var(--s1);
+  border-radius: var(--border-radius);
+  display: flex;
+  gap: var(--s0);
+  justify-content: center;
+  font-size: max(var(--s-1), 15px);
+  font-weight: 700;
+
+  box-shadow: 0 0 30px 0px var(--shadow-color);
+
+  position: relative;
+  &::before {
+    content: '';
+    position: absolute;
+    top: 0;
+    right: 20%;
+    bottom: 0;
+    left: 20%;
+    box-shadow: 0 0 50px 10px var(--shadow-color);
+    z-index: -1;
+  }
+
+  @media (min-width: 800px) {
+    margin-top: 0;
+    box-shadow: none;
+    &::before {
+      content: none;
+    }
+  }
+`
+
+const ClearCompleted = styled.div`
+  grid-area: clear;
+  display: flex;
+  justify-content: flex-end;
+  padding: var(--s0) var(--s1);
+  border-bottom-right-radius: var(--border-radius);
+  font-size: max(var(--s-1), 15px);
+  color: var(--muted-text-color);
 `
